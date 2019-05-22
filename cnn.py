18df5ea645c6450bb7bc714fb4410e26e70b3a5d
guilhermesantos <guilherme.milan9@gmail.com>
2019-05-22 01:04:35 -0300

    cuda error when attempting to move a large network to the gpu

--- cnn.py
+++ cnn.py
@@ -28,22 +28,48 @@ def load_dataset(batch_size):
 
 class CNN(nn.Module):
 	#Implementacao da AlexNet, baseada na implementacao do proprio pytorch
-	def __init__(self):
+	def __init__(self, num_classes=1000):
 		super(CNN, self).__init__()
-		self.conv1 = nn.Conv2d(3,6,5)#requires_grad=True por padrao
-		self.pool = nn.MaxPool2d(2,2)
-		self.conv2 = nn.Conv2d(6,16,5)
-		self.fc1 = nn.Linear(16*5*5, 120)
-		self.fc2 = nn.Linear(120, 84)
-		self.fc3 = nn.Linear(84, 10)
-		
+
+		self.features = nn.Sequential(
+			nn.Conv2d(3,64, kernel_size=11, stride=4, padding=2),#requires_grad=True por padrao
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+
+			nn.Conv2d(64, 192, kernel_size=5, padding=2),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+
+			nn.Conv2d(192, 384, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+
+			nn.Conv2d(384, 256, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+
+			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+		)
+
+		self.avg_pool = nn.AdaptiveAvgPool2d((6,6))
+
+		self.classifier = nn.Sequential(
+			nn.Dropout(),
+			nn.Linear(256*6*6, 4096),
+			nn.ReLU(inplace=True),
+			
+			nn.Dropout(),
+			nn.Linear(4096, 4096),
+			nn.ReLU(inplace=True),
+
+			nn.Linear(4096, num_classes),
+		)
+
 	def forward(self, x):
-		x = self.pool(F.relu(self.conv1(x))) #Aplica relu e faz pooling sobre a saida da 1a conv
-		x = self.pool(F.relu(self.conv2(x)))
-		x = x.view(-1, 16*5*5)
-		x = F.relu(self.fc1(x))
-		x = F.relu(self.fc2(x))
-		x = self.fc3(x)
+		x = self.features(x)
+		x = self.avgpool(x)
+		x = x.view(x.size(0), 256*6*6)
+		x = self.classifier(x)
 		return x
 
 def fit(model, train_dataset, device):
@@ -98,19 +124,32 @@ def validate(model, test_dataset, device):
 	return correct, total
 
 def main():
-	'''print("Cuda availability status:", torch.cuda.is_available())
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	print("Cuda availability status:", torch.cuda.is_available())
 	train, test, classes = load_dataset(16)
-	cnn = CNN()
-	cnn.to(device)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+	'''
+	print('setting device...')
+	torch.cuda.set_device(0)
+	print('device set')
+	print('instantiating model...')
+	cnn = CNN(len(classes))
+	print('sending to gpu')
+	cnn.cuda()
+	print('done')
 	print('training')
 	fit(cnn, train, device)
 	print('validating')
 	validate(cnn, test, device)'''
+
+	print('instantiating alexnet')
 	alexnet = models.alexnet(pretrained=True)
-	print('alexnet', alexnet)
-	for param in alexnet.parameters():
-		print('param', param)
+	print('sending alexnet to gpu')
+	alexnet.cuda()
+	print('done. validating')
+	'''validate(alexnet, test, device)
+	print('validated')'''
 	return 0
 
 if __name__ == '__main__':
